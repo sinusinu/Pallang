95007305cda296096c3e9b77ea2bab7b6de9f3a4
Woohyun Shin <cpu344@gmail.com>
2023-08-22 00:10:13 +0900

    Strip formatting when pasting text (probably)

---
+++ app/src/main/java/com/sinu/pallang/PlaintextifyingEditText.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020-2023 Woohyun Shin (sinusinu)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.sinu.pallang;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Build;
+import android.text.Spanned;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatEditText;
+
+// EditText that strips the formatting from pasted text
+// from https://stackoverflow.com/questions/24758698/paste-without-rich-text-formatting-into-edittext
+// wow nice name idiot
+public class PlaintextifyingEditText extends AppCompatEditText {
+    public PlaintextifyingEditText(@NonNull Context context) {
+        super(context);
+    }
+
+    public PlaintextifyingEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PlaintextifyingEditText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTextContextMenuItem(int id) {
+        if (id == android.R.id.paste) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                id = android.R.id.pasteAsPlainText;
+            } else {
+                onInterceptClipDataToPlainText();
+            }
+        }
+        return super.onTextContextMenuItem(id);
+    }
+
+
+    private void onInterceptClipDataToPlainText() {
+        ClipboardManager clipboard = (ClipboardManager) getContext()
+                .getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = clipboard.getPrimaryClip();
+        if (clip != null) {
+            for (int i = 0; i < clip.getItemCount(); i++) {
+                final CharSequence paste;
+                // Get an item as text and remove all spans by toString().
+                final CharSequence text = clip.getItemAt(i).coerceToText(getContext());
+                paste = (text instanceof Spanned) ? text.toString() : text;
+                if (paste != null) {
+                    copyToClipBoard(getContext(), paste);
+                }
+            }
+        }
+    }
+
+    private static void copyToClipBoard(@NonNull Context context, @NonNull CharSequence text) {
+        ClipData clipData = ClipData.newPlainText("rebase_copy", text);
+        ClipboardManager manager = (ClipboardManager) context
+                .getSystemService(Context.CLIPBOARD_SERVICE);
+        manager.setPrimaryClip(clipData);
+    }
+}
